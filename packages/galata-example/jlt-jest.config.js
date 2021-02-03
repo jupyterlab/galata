e44485fae064f1ad6e2485328bb68db618db1179
Mehmet Bektas <mbektas@bloomberg.net>
2021-02-03 12:22:42 -0800

    remove config files, since no more required

--- packages/galata-example/jlt-jest.config.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Bloomberg Finance LP.
-// Distributed under the terms of the Modified BSD License.
-
-// use config from @jupyterlab/galata
-module.exports = {
-    preset: './node_modules/@jupyterlab/galata/jest.config.js'
-};
